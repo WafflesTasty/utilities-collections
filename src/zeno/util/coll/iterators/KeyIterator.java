42fb7d3acfe3d40a5a500432db84355abe034944
X-Zeno-X <zenothedefiant@gmail.com>
2020-02-03 00:50:19 +0100

    Initial commit.

---
+++ src/zeno/util/coll/iterators/KeyIterator.java
@@ -0,0 +1,49 @@
+package zeno.util.coll.iterators;
+
+import java.util.Iterator;
+
+import zeno.util.coll.Dictionary;
+import zeno.util.coll.Dictionary.Pair;
+
+/**
+ * The {@code KeyIterator} defines an iterator over {@code Dictionary} keys.
+ *
+ * @author Zeno
+ * @since Feb 03, 2020
+ * @version 1.0
+ * 
+ * 
+ * @param <K>  a dictionary key type
+ * @param <V>  a dictionary value type
+ * @see Iterator
+ */
+public class KeyIterator<K, V> implements Iterator<K>
+{
+	private Iterator<Pair<K, V>> source;
+	
+	/**
+	 * Creates a new {@code KeyIterator}.
+	 * 
+	 * @param dic  a target dictionary
+	 * 
+	 * 
+	 * @see Dictionary
+	 */
+	public KeyIterator(Dictionary<K, V> dic)
+	{
+		source = dic.iterator();
+	}
+	
+	
+	@Override
+	public boolean hasNext()
+	{
+		return source.hasNext();
+	}
+
+	@Override
+	public K next()
+	{
+		return source.next().Key();
+	}
+}
---
+++ src/zeno/util/coll/iterators/ValueIterator.java
@@ -0,0 +1,49 @@
+package zeno.util.coll.iterators;
+
+import java.util.Iterator;
+
+import zeno.util.coll.Dictionary;
+import zeno.util.coll.Dictionary.Pair;
+
+/**
+ * The {@code KeyIterator} defines an iterator over {@code Dictionary} keys.
+ *
+ * @author Zeno
+ * @since Feb 03, 2020
+ * @version 1.0
+ * 
+ * 
+ * @param <K>  a dictionary key type
+ * @param <V>  a dictionary value type
+ * @see Iterator
+ */
+public class ValueIterator<K, V> implements Iterator<V>
+{
+	private Iterator<Pair<K, V>> source;
+	
+	/**
+	 * Creates a new {@code KeyIterator}.
+	 * 
+	 * @param dic  a target dictionary
+	 * 
+	 * 
+	 * @see Dictionary
+	 */
+	public ValueIterator(Dictionary<K, V> dic)
+	{
+		source = dic.iterator();
+	}
+	
+	
+	@Override
+	public boolean hasNext()
+	{
+		return source.hasNext();
+	}
+
+	@Override
+	public V next()
+	{
+		return source.next().Value();
+	}
+}
