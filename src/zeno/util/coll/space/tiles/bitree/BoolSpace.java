d12a1047a259eecc974476867b6eb4ef02c1803c
X-Zeno-X <zenothedefiant@gmail.com>
2021-11-27 19:32:13 +0100

    Added implementation of enum space for boolean values.

---
+++ src/zeno/util/coll/space/tiles/bitree/BoolSpace.java
@@ -0,0 +1,38 @@
+package zeno.util.coll.space.tiles.bitree;
+
+import zeno.util.coll.utilities.enums.Binary;
+
+/**
+ * The {@code BoolSpace} class defines an enum space with two possible values.
+ * For ease of use, the space considers {@code Binary.ZERO} as equal to null under the hood.
+ * For the sake of consistency, any null value is returned as {@code Binary.ZERO} as well.
+ *
+ * @author Waffles
+ * @since 27 Nov 2021
+ * @version 1.0
+ * 
+ * 
+ * @see BESpace
+ * @see Binary
+ */
+public class BoolSpace extends BESpace<Binary>
+{
+	@Override
+	public Binary put(Binary val, int... coords)
+	{
+		if(val != Binary.ZERO)
+		{
+			return super.put(val, coords);
+		}
+		
+		return super.put(null, coords);
+	}
+	
+	@Override
+	public Binary get(int... coords)
+	{
+		Binary b = super.get(coords);
+		if(b == null) return Binary.ZERO;
+		return super.get(coords);
+	}
+}
