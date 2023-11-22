f18c4a5118c5bbee9bfc1a7458cca4d6bd4b2f24
WafflesTasty <zenothedefiant@gmail.com>
2023-11-22 07:45:14 +0100

    Added ObjectSet class to arrays 1.1.

---
+++ src/waffles/utils/sets/arrays/set/ObjectSet.java
@@ -0,0 +1,32 @@
+package waffles.utils.sets.arrays.set;
+
+import waffles.utils.sets.arrays.ArraySet;
+import waffles.utils.sets.arrays.like.ObjectArray;
+
+/**
+ * An {@code ObjectSet} maintains an object array as an {@code ArraySet} object.
+ *
+ * @author Waffles
+ * @since 13 Nov 2023
+ * @version 1.1
+ * 
+ * 
+ * @param <O>  an object type
+ * @see ObjectArray
+ * @see ArraySet
+ */
+@FunctionalInterface
+public interface ObjectSet<O> extends ArraySet<Object[], O>, ObjectArray<O>
+{
+	@Override
+	public default int DataSize()
+	{
+		return Count();
+	}
+	
+	@Override
+	public default int Count()
+	{
+		return Array().length;
+	}
+}
